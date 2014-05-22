efa26e6f3c54a2a853ba96549bb1e39a873ba079
sadahanu <yuz04ster@gmail.com>
2014-05-21 22:17:37 -0400

    Create fof.reduce.py

---
+++ fof.reduce.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Sadahanu 2014
+import sys, string
+prevline = " ";
+flag = 0;
+for line in sys.stdin:
+    if prevline ==" " :
+        prevline = line
+        continue
+    elif line == prevline:
+          if flag == 0:
+             line3 = line.split()
+             print('<'+line3[0]+', '+line3[1]+', '+line3[2]+'>')
+             print('<'+line3[1]+', '+line3[0]+', '+line3[2]+'>')
+             print('<'+line3[2]+', '+line3[1]+', '+line3[0]+'>')
+             flag = 1
+             prevline = line
+          else:
+             prevline = line
+    else:
+          flag = 0;
+          prevline = line;
