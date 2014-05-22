d6b265285d7b84a2c18c6bbedc3b0fecfa6b96ec
sadahanu <yuz04ster@gmail.com>
2014-05-21 22:44:37 -0400

    Create fof.mapper.py

---
+++ Three-s-Company/fof.mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys, string
+for line in sys.stdin:
+    frlist = string.split(line)
+    if len(frlist)<3:
+        break
+    for i in range (1,len(frlist)-1):
+        for j in range(i+1, len(frlist)):
+            #tri = [int(frlist[0]),int(frlist[i]),int(frlist[j])]
+            tri = [frlist[0],frlist[i],frlist[j]]
+            stri = sorted(tri)
+            print(' '.join(stri))
